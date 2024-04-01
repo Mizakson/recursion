e5674f77ce43e4a0a58bcde8ed863d7922cdabdf
Mizakson <maxwellizakson@gmail.com>
2024-04-01 13:08:21 -0400

    Change file name -> fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -12,3 +12,15 @@ function fibs(num) {
 
 // console.log(fibs(8))
 // [0, 1, 1, 2, 3, 5, 8, 13]
+
+
+// recursive fibonacci sequence
+function fibsRec(num) {
+    
+}
+
+console.log(fibsRec(0))
+console.log(fibsRec(1))
+console.log(fibsRec(2))
+console.log(fibsRec(8))
+// [0, 1, 1, 2, 3, 5, 8, 13]
