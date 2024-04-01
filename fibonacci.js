ea0848ce3cc985513d55ae5f0867a208939c8896
Mizakson <maxwellizakson@gmail.com>
2024-04-01 13:16:44 -0400

    Change fibsRec -> noArrFibsRec
    not complete solution
    used from site

--- fibonacci.js
+++ fibonacci.js
@@ -14,13 +14,15 @@ function fibs(num) {
 // [0, 1, 1, 2, 3, 5, 8, 13]
 
 
-// recursive fibonacci sequence
-function fibsRec(num) {
-    
+// recursive fibonacci sequence without array in solution
+function noArrFibsRec(num) {
+   if (num == 0) return 0
+   if (num == 1) return 1
+   return noArrFibsRec(num - 1) + noArrFibsRec(num - 2)
 }
 
-console.log(fibsRec(0))
-console.log(fibsRec(1))
-console.log(fibsRec(2))
-console.log(fibsRec(8))
+console.log(noArrFibsRec(0))
+console.log(noArrFibsRec(1))
+console.log(noArrFibsRec(2))
+console.log(noArrFibsRec(8))
 // [0, 1, 1, 2, 3, 5, 8, 13]
