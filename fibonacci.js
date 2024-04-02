5c599a21ea38a16cc538bda84a353dd67b197008
Mizakson <maxwellizakson@gmail.com>
2024-04-02 09:46:58 -0400

    Add working iterable solution -> fibs

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,26 @@
 // non recursive fibonacci sequence
+/* 
+have an array of first two terms [0,1]
+
+for loop
+if the iterated value is less than the arg
+return that many numbers in the fibonacci sequence
+*/
 function fibs(num) {
+    var arr = [0, 1]
+
+    for (let i = 2; i < num; i++) {
+        let prev = arr[i - 1]
+        let prevTwo = arr[i - 2]
+        let next = prev + prevTwo
+        arr.push(next)
+    }
+
+    return arr
 
 }
 
-// console.log(fibs(8))
+console.log(fibs(8))
 // [0, 1, 1, 2, 3, 5, 8, 13]
 
 
