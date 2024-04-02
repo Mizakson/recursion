0c02b626b842e518a0557e397e9ad706f990c6f8
Mizakson <maxwellizakson@gmail.com>
2024-04-02 09:39:37 -0400

    Empty function body
    start whiteboarding -- pseudocode

--- fibonacci.js
+++ fibonacci.js
@@ -1,13 +1,6 @@
 // non recursive fibonacci sequence
 function fibs(num) {
 
-    const arr = [0, 1]
-
-    for (let i = 2; i < num; i++) {
-        arr[i] = arr[i - 1] + arr[i - 2]
-    }
-
-    return arr
 }
 
 // console.log(fibs(8))
@@ -15,12 +8,9 @@ function fibs(num) {
 
 
 // recursive fibonacci sequence without array in solution
-// solution from https://www.scaler.com/topics/fibonacci-series-in-javascript/
-function noArrFibsRec(num) {
-   if (num == 0) return 0
-   if (num == 1) return 1
-   return noArrFibsRec(num - 1) + noArrFibsRec(num - 2)
+function fibsRec(num) {
+
 }
 
-console.log(noArrFibsRec(8))
+// console.log(fibsRec(8))
 // [0, 1, 1, 2, 3, 5, 8, 13]
