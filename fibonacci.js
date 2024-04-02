16e069414a6f06255ce941a28144da8685a4ace5
Mizakson <maxwellizakson@gmail.com>
2024-04-02 10:23:04 -0400

    Finish recursive solution
    use of spread operator

--- fibonacci.js
+++ fibonacci.js
@@ -20,14 +20,33 @@ function fibs(num) {
 
 }
 
-console.log(fibs(8))
+// console.log(fibs(8))
 // [0, 1, 1, 2, 3, 5, 8, 13]
 
 
-// recursive fibonacci sequence without array in solution
+// recursive fibonacci sequence solution
+/* 
+base:
+if arr len = 0 return 0
+if arr len = 1 return 1
+
+recursive:
+if arr len >= 2
+prev = f(n -1) + f(n - 2)
+arr.push(prev)
+return arr
+
+*/
 function fibsRec(num) {
 
+    if (num == 0) return [0]
+    if (num == 1) return [0, 1]
+    const arr = fibsRec(num - 1)
+    // return copy of main arr (arr), next value is f(n-1) + f(n-2)
+    // use of spread operator (...) to copy arr
+    return [...arr, arr[num - 1] + arr[num - 2]]
+    
 }
 
-// console.log(fibsRec(8))
+console.log(fibsRec(8))
 // [0, 1, 1, 2, 3, 5, 8, 13]
