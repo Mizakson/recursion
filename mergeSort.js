1d9759dc15e858a42734a922fe3662c0b9c8b031
Mizakson <maxwellizakson@gmail.com>
2024-04-08 12:22:28 -0400

    Edit mergeSort.js

--- mergeSort.js
+++ mergeSort.js
@@ -1,8 +1,28 @@
 // fibonacci merge sort
 function mergeSort(arr) {
 
+    /* 
+    find mid point of arr
+
+    if lowest < highest -- arr.length >= 2
+        f(low, midpoint)
+        f(midpoint + 1, high)
+        merge(both arrays)
+    
+    */
+    let result = []
+    let mid = Math.ceil(arr.length / 2)
+    let left = arr.slice(0, mid)
+    let right = arr.slice(mid, arr.length)
+
+    while (mid != 1) {
+        return [mergeSort(left), mergeSort(right)]
+    }
+
+    return arr
+
 }
 
-// let testArr = [3, 2, 1, 13, 8, 5, 0, 1]
-// console.log(mergeSort(testArr))
+let testArr = [3, 2, 1, 13, 8, 5, 0, 1]
+console.log(mergeSort(testArr))
 // [0, 1, 1, 2, 3, 5, 8, 13]
