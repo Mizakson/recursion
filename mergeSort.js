b665953d42f0c41fb809158649dd3d830f214073
Mizakson <maxwellizakson@gmail.com>
2024-04-09 14:36:53 -0400

    edit mergeSort.js

--- mergeSort.js
+++ mergeSort.js
@@ -1,38 +1,42 @@
 // fibonacci merge sort
 function mergeSort(arr) {
 
-    /* 
-    find mid point of arr
+/* 
+helper function to sort a list
 
-    if lowest < highest -- arr.length >= 2
-        f(low, midpoint)
-        f(midpoint + 1, high)
-        merge(both arrays)
-    
-    */
+return mergeFunc(arr1,arr2)
 
-
-    let low = 1
-    let high = arr.length
-    let result = []
-    
-    if (low < high) {
-        let mid = high / 2
-        let left = arr.slice(0,mid)
-        let right = arr.slice(mid,high)
-
-        while (left.length > 1) {
-            return mergeSort(left)
-        }
-
-        while (right.length > 1) {
-            return mergeSort(right)
-        }
-
-        return [left,right]
+*/
+    let layer2, layer3
+    function merge(arr1,arr2) {
+        let arr3 = []
+        return [arr1,arr2]
     }
 
-    // return result
+    let left,right
+    if (arr.length > 1) {
+        
+        let m = Math.floor(arr.length / 2)
+        const [left,right] = [arr.slice(0,m),arr.slice(m,arr.length)]
+
+        while (left.length > 1 && right.length > 1) {
+            layer2 = [mergeSort(left),mergeSort(right)]
+            return layer2
+        }
+
+        if ((left.length === 1) && (right.length === 1)) {
+            layer3 = [left[0],right[0]]
+            if (left > right) {
+                layer3.reverse()
+            }
+            // console.log(layer3[0],layer3[1])
+            return layer3
+        }
+
+    }
+
+    return arr
+    // return arr
 
 }
 
